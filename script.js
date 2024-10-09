9f144e1b9e06c59645ff4972b447cb85eb53154d
Pluton-ffg <roccohatkeinecodes@gmail.com>
2024-10-09 16:52:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('btnCicloVida').addEventListener('click', function() {
+    window.open('https://www.canva.com/design/DAGTG9p1lz0/WEcFRwd8xH4NDuTtbARftQ/edit?utm_content=DAGTG9p1lz0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+});
+
+document.getElementById('btnMutacion').addEventListener('click', function() {
+    window.open('https://www.canva.com/design/DAGS7CVshmQ/BfKt7vf4ZVNeY5XUMlDVLw/edit?utm_content=DAGS7CVshmQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+});
+
+document.getElementById('instagramButton').addEventListener('click', function() {
+    window.open('https://www.instagram.com/extorssivo/', '_blank');
+});
